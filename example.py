822a1fe0d9c63e6ade5a9c836d2ac533c89eead5
David Buchmann <david.buchmann@gmail.com>
2024-08-01 00:10:29 +0200

    client: Add ::input to run

--- example.py
+++ example.py
@@ -4,8 +4,10 @@ from spot import Client
 
 c = Client(os.getenv("SPOT_API_KEY"))
 workflows = c.list_workflows()
+workflow = [w for w in workflows if w.data.name == "sanity test"][0]
 print(f"Workflow: {workflows[0].data.id} -> {workflows[0].inputs[0].node_config.label}")
+print(workflows[0].data)
 input(f"Run: {workflows[0].data.name} (press enter)")
-o = workflows[0].run({workflows[0].inputs[0].id: sys.argv[1]})
+o = workflows[0].run({f"{workflows[0].inputs[0].id}::input": sys.argv[1]})
 print(f"Started run... {o.data['id']}")
 print(o.wait())
