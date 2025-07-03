305c9650601054ec66c053e762e25b42c846f6e5
David Buchmann <david.buchmann@gmail.com>
2025-07-03 15:35:00 +0200

    workflows: Make an alias for ConfigError

--- noxus_sdk/workflows/__init__.py
+++ noxus_sdk/workflows/__init__.py
@@ -1,2 +1,6 @@
-from noxus_sdk.workflows.workflow import WorkflowDefinition, load_node_types
+from noxus_sdk.workflows.workflow import (
+    WorkflowDefinition,
+    load_node_types,
+    ConfigError,
+)
 from noxus_sdk.workflows.agentflow import AgentFlowDefinition
